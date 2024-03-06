19c226eda792edd5ff90f3ed4e4c10d254df5f73
Fabian Aguirre <aguirrefabian.unne@gmail.com>
2024-03-06 13:20:01 -0300

    modify the comminite page and options of browse-by section.

--- src/themes/riunne/app/browse-by/browse-by-title-page/browse-by-title-page.component.ts
+++ src/themes/riunne/app/browse-by/browse-by-title-page/browse-by-title-page.component.ts
@@ -4,14 +4,17 @@ import { BrowseByTitlePageComponent as BaseComponent } from '../../../../../app/
 @Component({
   selector: 'ds-browse-by-title-page',
   // styleUrls: ['./browse-by-title-page.component.scss'],
-  styleUrls: ['../../../../../app/browse-by/browse-by-metadata-page/browse-by-metadata-page.component.scss'],
+  //styleUrls: ['../../../../../app/browse-by/browse-by-metadata-page/browse-by-metadata-page.component.scss'],
   // templateUrl: './browse-by-title-page.component.html'
-  templateUrl: '../../../../../app/browse-by/browse-by-metadata-page/browse-by-metadata-page.component.html'
+  //templateUrl: '../../../../../app/browse-by/browse-by-metadata-page/browse-by-metadata-page.component.html'
+  styleUrls: [
+    '../browse-by-metadata-page/browse-by-metadata-page.component.scss',
+  ],
+  templateUrl:
+    '../browse-by-metadata-page/browse-by-metadata-page.component.html',
 })
 
 /**
  * Component for determining what Browse-By component to use depending on the metadata (browse ID) provided
  */
-
-export class BrowseByTitlePageComponent extends BaseComponent {
-}
+export class BrowseByTitlePageComponent extends BaseComponent {}
--- src/themes/riunne/app/shared/comcol-page-browse-by/comcol-page-browse-by.component.ts
+++ src/themes/riunne/app/shared/comcol-page-browse-by/comcol-page-browse-by.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ComcolPageBrowseByComponent as BaseComponent} from '../../../../../app/shared/comcol/comcol-page-browse-by/comcol-page-browse-by.component';
+import { ComcolPageBrowseByComponent as BaseComponent } from '../../../../../app/shared/comcol/comcol-page-browse-by/comcol-page-browse-by.component';
 
 /**
  * A component to display the "Browse By" section of a Community or Collection page
@@ -7,9 +7,9 @@ import { ComcolPageBrowseByComponent as BaseComponent} from '../../../../../app/
  */
 @Component({
   selector: 'ds-comcol-page-browse-by',
-  // styleUrls: ['./comcol-page-browse-by.component.scss'],
-  styleUrls: ['../../../../../app/shared/comcol/comcol-page-browse-by/comcol-page-browse-by.component.scss'],
-  // templateUrl: './comcol-page-browse-by.component.html'
-  templateUrl: '../../../../../app/shared/comcol/comcol-page-browse-by/comcol-page-browse-by.component.html'
+  styleUrls: ['./comcol-page-browse-by.component.scss'],
+  //styleUrls: ['../../../../../app/shared/comcol/comcol-page-browse-by/comcol-page-browse-by.component.scss'],
+  templateUrl: './comcol-page-browse-by.component.html',
+  //templateUrl: '../../../../../app/shared/comcol/comcol-page-browse-by/comcol-page-browse-by.component.html'
 })
 export class ComcolPageBrowseByComponent extends BaseComponent {}
